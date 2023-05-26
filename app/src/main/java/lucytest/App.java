375bb53d31668ee3a0c5c8c7c20bee8f67bbc4ed
absolverg <absolverg@gmail.com>
2023-05-26 17:20:08 +0900

    gitignore 변경 & embed 비허용 & src는 유튜브만

--- app/src/main/java/lucytest/App.java
+++ app/src/main/java/lucytest/App.java
@@ -13,7 +13,7 @@ public class App {
     public static void main(String[] args) {
         XssSaxFilter filter = XssSaxFilter.getInstance("lucy-xss-sax.xml", false);
         // String dirty = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/CbtdD5XGpmg\" style=\"width: auto;\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe>";
-        String dirty = "<img src=\"https://image.wemakeprice.com\" style=\"width: auto;\" />";
+        String dirty = "<a src=\"https://image.wemakeprice.com\" style=\"width: auto;\">test</a>";
         String clean = filter.doFilter(dirty);
         System.out.println(clean);
         System.out.println(dirty.equals(clean));
